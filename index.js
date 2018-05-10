a6e9be0cfbc3bed324701c417d145b57b97a16fa
dave.w.burns@gmail.com <dave.w.burns@gmail.com>
2018-05-10 15:52:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ function divideBy3() {
 function add(a, b) {
   return a+b;
 }
+
+function subtract(a, b) {
+  return a-b;
+}
+
+function multiply(a, b) {
+  return a*b;
+}
+
+function divide(a, b) {
+  return a/b;
+}
